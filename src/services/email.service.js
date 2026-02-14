cb5b1cbb3555253630f46868e6cf4b961c01e951
panditdhamdhere <dhamdherepandit@gmail.com>
2026-02-14 17:53:11 +0530

    func: new function for mail added

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    type: 'OAuth2',
+    type: "OAuth2",
     user: process.env.EMAIL_USER,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -15,10 +15,38 @@ const transporter = nodemailer.createTransport({
 // Verify the connection configuration
 transporter.verify((error, success) => {
   if (error) {
-    console.error('Error connecting to email server:', error);
+    console.error("Error connecting to email server:", error);
   } else {
-    console.log('Email server is ready to send messages');
+    console.log("Email server is ready to send messages");
   }
 });
 
-module.exports = transporter;
+// Function to send email
+const sendEmail = async (to, subject, text, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"ledger" <${process.env.EMAIL_USER}>`, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+async function sendRegistrationEmail(userEmail, name) {
+  const subject = "Welcome to ledger!";
+  const text = `Hello ${name}, \n\nThank you for registering ledger, We are excited to have you on board!n\n\ The ledger team`;
+  const html = `<p>Hello ${name}, \n\nThank you for registering ledger, We are excited to have you on board!n\n\ The ledger team</p>`;
+
+  await sendEmail(userEmail, subject, text, html);
+}
+
+module.exports = {
+  sendRegistrationEmail,
+};
