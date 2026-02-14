2e8ed8ae050832cc7b86197fe09373637e0b2674
panditdhamdhere <dhamdherepandit@gmail.com>
2026-02-14 16:25:36 +0530

    fix: small bug fixed

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,8 @@
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 
+// user register controller
+// post - /api/auth/register
 async function userRegisterController(req, res) {
   const { email, password, name } = req.body;
 
@@ -35,6 +37,44 @@ async function userRegisterController(req, res) {
   });
 }
 
+// user login controller
+// post - /api/auth/login
+async function userLoginController(req, res) {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email }).select("+password")
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Email or password is INVALID",
+    });
+  }
+
+  const isValidPassword = await user.comparePassword(password);
+
+  if (!isValidPassword) {
+    return res.status(401).json({
+      message: "Email or password is INVALID",
+    });
+  }
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "3d",
+  });
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
+}
+
 module.exports = {
   userRegisterController,
+  userLoginController,
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,10 @@ const authController = require("../controllers/auth.controllers");
 
 const router = express.Router();
 
+// Post : /api/auth/register
 router.post("/register", authController.userRegisterController);
+
+// Post : /api/auth/login
+router.post("/login", authController.userLoginController);
 
 module.exports = router;
