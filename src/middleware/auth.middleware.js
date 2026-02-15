476dc6d8d9b3d90530d3c9b2ffef336de8760ac7
panditdhamdhere <dhamdherepandit@gmail.com>
2026-02-15 15:22:06 +0530

    auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+const userModel = require("../models/user.model");
+const Jwt = require("jsonwebtoken");
+
+async function authMiddleware(req, res, next) {
+  const token = req.cookies.token || req.headers.authorization?.spilt(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized access, token is missing",
+    });
+  }
+
+  try {
+    const decoded = Jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    const user = await userModel.findById(decoded.userId);
+
+    req.user = user;
+    return next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unathorized access, Token is invalid",
+    });
+  }
+}
